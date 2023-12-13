c152f79e74790b11f1dc1deb579f1aa6cfd13b18
coderyu5523 <coderyu5523@gmail.com>
2023-12-13 15:25:34 +0900

    예제 수정

--- src/ex02/TemperEx01.java
+++ src/ex02/TemperEx01.java
@@ -4,23 +4,28 @@ import java.util.Scanner;
 
 public class TemperEx01 {
     public static void main(String[] args) {
+
+        double cho, ins, temp;
+
         Scanner sc = new Scanner(System.in);
+        System.out.println("==============");
         System.out.println("1.화씨 -> 섭씨");
         System.out.println("2.섭씨 -> 화씨");
-        double n, x, y;
-        n = sc.nextInt();
+        System.out.println("==============");
+
+        cho = sc.nextInt();
 
 
-        if (n == 1) {
+        if (cho == 1) {
             System.out.print(("화씨 온도를 입력하세요 :"));
-            x = sc.nextInt();
-            y = (x - 32) * 5 / 9;
-            System.out.println("섭씨 온도는" + y + "도 입니다");
-        } else if (n == 2) {
+            ins = sc.nextInt();
+            temp = (ins - 32) * 5 / 9;
+            System.out.println("섭씨 온도는" + temp + "도 입니다");
+        } else if (cho == 2) {
             System.out.print(("섭씨 온도를 입력하세요 :"));
-            x = sc.nextInt();
-            y = x * 9 / 5 + 32;
-            System.out.println("화씨 온도는" + y + "도 입니다");
+            ins = sc.nextInt();
+            temp = ins * 9 / 5 + 32;
+            System.out.println("화씨 온도는" + temp + "도 입니다");
 
         }
 
