cd003be179800b9e0d15bd0fc3cd6b7b4d400e7c
coderyu5523 <coderyu5523@gmail.com>
2023-12-13 11:12:32 +0900

    상수

---
+++ src/ex01/CoustEx01.java
@@ -0,0 +1,11 @@
+package ex01;
+
+public class CoustEx01 {
+    public static void main(String[] args) {
+        int num = 1;  // 변수
+        final int sum = 10; // 상수 , 변경 불가능 read only memory
+        num = 2;
+        //sum = 2;
+
+    }
+}
---
+++ src/ex01/VarEx01.java
@@ -0,0 +1,9 @@
+package ex01;
+
+public class VarEx01 {
+
+    public static void main(String[] args) {
+        
+
+    }
+}
