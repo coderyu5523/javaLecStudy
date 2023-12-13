d0da7c63e8d9b2caf38d58596ff16135632d797d
coderyu5523 <coderyu5523@gmail.com>
2023-12-13 11:39:14 +0900

    변수이름

---
+++ src/ex01/IdentiEx01.java
@@ -0,0 +1,15 @@
+package ex01;
+
+public class IdentiEx01 {
+
+    int abc__ ; // 되는데 절대 사용 안함, 변수를 표현하는 표기법은
+
+    int user_money ; // 절대 사용하지 않음
+    int userMoney ;  // 변수 표기법은 카멜표기법
+
+    int abc# ;
+    int method ;
+    double abc$$ ; // 특수문자 사용하지말자
+    int 1stMoney ; //  숫자로 변수 시작 못함
+
+}
