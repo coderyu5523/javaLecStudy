5ab4ee41cc537b8d76186c101b9b6618da68044a
coderyu5523 <coderyu5523@gmail.com>
2023-12-14 14:53:44 +0900

    구구단 4

--- src/ex03/GugudanEx01.java
+++ src/ex03/GugudanEx01.java
@@ -5,95 +5,21 @@ public class GugudanEx01 {
     //1~9단까지 출력되는 프로그램을 만드세요
     // 스캐너를 이용하여 입락받은 단만 출력하시오.
     public static void main(String[] args) {
-        int x;
-        x = 2;
-        System.out.println(x + "*1=" + (x * 1));
-        System.out.println(x + "*2=" + (x * 2));
-        System.out.println(x + "*3=" + (x * 3));
-        System.out.println(x + "*4=" + (x * 4));
-        System.out.println(x + "*5=" + (x * 5));
-        System.out.println(x + "*6=" + (x * 6));
-        System.out.println(x + "*7=" + (x * 7));
-        System.out.println(x + "*8=" + (x * 8));
-        System.out.println(x + "*9=" + (x * 9));
-        System.out.println();
-        x = 3;
-        System.out.println(x + "*1=" + (x * 1));
-        System.out.println(x + "*2=" + (x * 2));
-        System.out.println(x + "*3=" + (x * 3));
-        System.out.println(x + "*4=" + (x * 4));
-        System.out.println(x + "*5=" + (x * 5));
-        System.out.println(x + "*6=" + (x * 6));
-        System.out.println(x + "*7=" + (x * 7));
-        System.out.println(x + "*8=" + (x * 8));
-        System.out.println(x + "*9=" + (x * 9));
-        System.out.println();
-        x = 4;
-        System.out.println(x + "*1=" + (x * 1));
-        System.out.println(x + "*2=" + (x * 2));
-        System.out.println(x + "*3=" + (x * 3));
-        System.out.println(x + "*4=" + (x * 4));
-        System.out.println(x + "*5=" + (x * 5));
-        System.out.println(x + "*6=" + (x * 6));
-        System.out.println(x + "*7=" + (x * 7));
-        System.out.println(x + "*8=" + (x * 8));
-        System.out.println(x + "*9=" + (x * 9));
-        System.out.println();
-        x = 5;
-        System.out.println(x + "*1=" + (x * 1));
-        System.out.println(x + "*2=" + (x * 2));
-        System.out.println(x + "*3=" + (x * 3));
-        System.out.println(x + "*4=" + (x * 4));
-        System.out.println(x + "*5=" + (x * 5));
-        System.out.println(x + "*6=" + (x * 6));
-        System.out.println(x + "*7=" + (x * 7));
-        System.out.println(x + "*8=" + (x * 8));
-        System.out.println(x + "*9=" + (x * 9));
-        System.out.println();
-        x = 6;
-        System.out.println(x + "*1=" + (x * 1));
-        System.out.println(x + "*2=" + (x * 2));
-        System.out.println(x + "*3=" + (x * 3));
-        System.out.println(x + "*4=" + (x * 4));
-        System.out.println(x + "*5=" + (x * 5));
-        System.out.println(x + "*6=" + (x * 6));
-        System.out.println(x + "*7=" + (x * 7));
-        System.out.println(x + "*8=" + (x * 8));
-        System.out.println(x + "*9=" + (x * 9));
-        System.out.println();
-        x = 7;
-        System.out.println(x + "*1=" + (x * 1));
-        System.out.println(x + "*2=" + (x * 2));
-        System.out.println(x + "*3=" + (x * 3));
-        System.out.println(x + "*4=" + (x * 4));
-        System.out.println(x + "*5=" + (x * 5));
-        System.out.println(x + "*6=" + (x * 6));
-        System.out.println(x + "*7=" + (x * 7));
-        System.out.println(x + "*8=" + (x * 8));
-        System.out.println(x + "*9=" + (x * 9));
-        System.out.println();
-        x = 8;
-        System.out.println(x + "*1=" + (x * 1));
-        System.out.println(x + "*2=" + (x * 2));
-        System.out.println(x + "*3=" + (x * 3));
-        System.out.println(x + "*4=" + (x * 4));
-        System.out.println(x + "*5=" + (x * 5));
-        System.out.println(x + "*6=" + (x * 6));
-        System.out.println(x + "*7=" + (x * 7));
-        System.out.println(x + "*8=" + (x * 8));
-        System.out.println(x + "*9=" + (x * 9));
-        System.out.println();
-        x = 9;
-        System.out.println(x + "*1=" + (x * 1));
-        System.out.println(x + "*2=" + (x * 2));
-        System.out.println(x + "*3=" + (x * 3));
-        System.out.println(x + "*4=" + (x * 4));
-        System.out.println(x + "*5=" + (x * 5));
-        System.out.println(x + "*6=" + (x * 6));
-        System.out.println(x + "*7=" + (x * 7));
-        System.out.println(x + "*8=" + (x * 8));
-        System.out.println(x + "*9=" + (x * 9));
-        System.out.println();
+
+        for (int x = 2; x <= 9; x++) {
+
+            System.out.println(x + "*1=" + (x * 1));
+            System.out.println(x + "*2=" + (x * 2));
+            System.out.println(x + "*3=" + (x * 3));
+            System.out.println(x + "*4=" + (x * 4));
+            System.out.println(x + "*5=" + (x * 5));
+            System.out.println(x + "*6=" + (x * 6));
+            System.out.println(x + "*7=" + (x * 7));
+            System.out.println(x + "*8=" + (x * 8));
+            System.out.println(x + "*9=" + (x * 9));
+            System.out.println();
+
+        }
     }
 
 
