0930ee05a880dc2afadbcd5be07be164655b002c
coderyu5523 <coderyu5523@gmail.com>
2023-12-20 08:58:59 +0900

    1

---
+++ src/ex04/MethodEX04.java
@@ -0,0 +1,20 @@
+package ex04;
+
+public class MethodEX04 {
+
+    void m1() {
+        int n1 = 1;
+        static int n2 = 3;
+    }
+
+    static void m2() {
+        int n3 = 4;
+    }
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
