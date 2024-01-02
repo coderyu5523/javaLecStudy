f8aafd2d415042c185f4d6209e2f124460a5bab9
coderyu5523 <coderyu5523@gmail.com>
2024-01-02 09:31:11 +0900

    ã리팩토링

--- src/Hello.java
+++ src/Hello.java
@@ -3,8 +3,7 @@ public class Hello {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
-        System.out.println("4");
-        System.out.println("5");
+
     }
 
 }
