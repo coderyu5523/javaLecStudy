d5d25a7a58fa19012c0208359c47fee84be5dec5
coderyu5523 <coderyu5523@gmail.com>
2024-01-05 16:11:58 +0900

    스트림연습

--- src/ex14/example1/StreamEx01.java
+++ src/ex14/example1/StreamEx01.java
@@ -1,16 +1,14 @@
 package ex14.example1;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class StreamEx01 {
     public static void main(String[] args) {
 
         List<String> list = Arrays.asList("park", "kim", "lee");
-        Stream<String> s2 = list.stream().sorted(Comparator.reverseOrder());
-        s2.forEach(System.out::println);
+        List<String> s2 = list.stream().filter(s -> s.contains("p")).toList();
+        System.out.println(s2);
 
 
     }
